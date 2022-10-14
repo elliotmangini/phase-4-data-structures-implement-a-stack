01481c3a765b0570764f1ddf2be232d459bdda3e
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-14 10:06:31 -0600

    commit tree eace04c288b0d3694d14337fa528a0e5dee0686d

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,21 @@
 # your code here
+class Stack
+    
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+        length = @stack.length
+        @stack[length] = value
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length - 1]
+    end
+
+end
